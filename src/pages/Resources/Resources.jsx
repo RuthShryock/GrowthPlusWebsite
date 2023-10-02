18b1078bf30ecaf710de68453e1817e936049459
RuthShryock <ruthieshryock@gmail.com>
2023-10-02 10:38:36 -0400

    adding tutorial vid link

--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {faGooglePlay} from '@fortawesome/free-brands-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
 import Helmet from "react-helmet";
 import React from 'react';
 import './Resources.css';
@@ -18,9 +19,11 @@ const Resources = () => {
                     <div className="grid-child-resources" id='buttonOne'><a href='mailto:contactarroseplus@gmail.com' target="_blank" rel="noreferrer"><button className="iconBtn"><FontAwesomeIcon icon={faEnvelope}/></button></a></div>
                     <div className="grid-child-resources" id='buttonTwo'><a href="https://github.com/ucfcs/GrowthPlus" target="_blank" rel="noreferrer"><button className="iconBtn"><FontAwesomeIcon icon={faGithub}/></button></a></div>
                     <div className="grid-child-resources" id='buttonThree'><a href='https://play.google.com/store/apps/details?id=com.GrowthPlus&pli=1' target="_blank" rel="noreferrer"><button className="iconBtn"><FontAwesomeIcon icon={faGooglePlay}/></button></a></div>
+                    <div className="grid-child-resources" id='buttonFour'><a href='https://youtu.be/X1B1j9lDhl8?si=3A8vcgy3_7DcaSuh' target="_blank" rel="noreferrer"><button className="iconBtn"><FontAwesomeIcon icon={faYoutube}/></button></a></div>
                     <div className="grid-child-resources" id='textOne'><h3>contact us at:<br/>contactarroseplus@<br/>gmail.com</h3></div>
                     <div className="grid-child-resources" id='textTwo'><h3>check out our code</h3></div>
                     <div className="grid-child-resources" id='textThree'><h3>download the app</h3></div>
+                    <div className="grid-child-resources" id='textFour'><h3>video tutorial</h3></div>
                 </div>
             </div>
         </div>   
